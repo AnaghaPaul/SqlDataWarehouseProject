43465fed279755e45f1f27e39d64d1b541a227c4
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-01-20 13:36:39 -0500

    Update proc_load_cleaning_silver.sql

--- scripts/proc_load_cleaning_silver.sql
+++ scripts/proc_load_cleaning_silver.sql
@@ -32,3 +32,35 @@ SELECT TOP(20) *
 FROM silver.crm_cust_info;
 SELECT TOP(20) * 
 FROM bronze.crm_cust_info;
+
+
+---crm_prd_info
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_name,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+
+SELECT 
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key,7,LEN(prd_key)) as prd_key,
+prd_name,
+ISNULL(prd_cost,0) as prd_cost,
+ CASE UPPER(TRIM(prd_line)) 
+      WHEN 'M' THEN 'Mountain'
+	  WHEN 'R' THEN 'Road'
+	  WHEN 'S' THEN 'Other Sales'
+	  WHEN 'T' THEN 'Touring'
+	  ELSE 'n/a'
+ END AS prd_line,
+ CAST(prd_start_dt AS DATE) as prd_start_dt,
+--LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_test,--does not work on sql server
+CAST(DATEADD(DAY,-1,LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) AS prd_end_dt
+FROM
+bronze.crm_prd_info;
