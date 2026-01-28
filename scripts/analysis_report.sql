57a8b418b526b369cdd7806199fd07d68c2c2588
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-01-28 15:07:07 -0500

    Update analysis_report.sql

--- scripts/analysis_report.sql
+++ scripts/analysis_report.sql
@@ -37,6 +37,47 @@ WHERE y2013.order_year = 2013;
 /*result : 1.88% < 40% --> E-Commerce type is aquisition mode, the buyers of this business usually do not repurchase the product,
 this may be due to the business selling one-time purchase products and customer does not need to up-grade soon.
 Loyalty programs are not good long-term investments, instead the company need to focus on acquiring new customers*/
+WITH customer_metrics AS (
+    SELECT
+        customer_key,
+        MIN(YEAR(order_date)) AS acquisition_year,
+        COUNT(DISTINCT order_number) AS no_of_orders,
+        SUM(sales_amount) AS customer_lifetime_value
+    FROM gold.fact_sales
+    GROUP BY customer_key
+)
+SELECT 
+dim_customers.customer_id,
+dim_customers.customer_number,
+dim_customers.first_name,
+dim_customers.last_name,
+dim_customers.country,
+dim_customers.marital_status,
+dim_customers.birthdate,
+customer_metrics.acquisition_year,
+customer_metrics.no_of_orders,
+customer_metrics.customer_lifetime_value
+FROM gold.dim_customers AS dim_customers
+LEFT JOIN
+customer_metrics
+ON
+dim_customers.customer_key =customer_metrics.customer_key
+ORDER BY dim_customers.customer_key;
+
+
+/*
+customer_id	customer_number	first_name	last_name	country	marital_status	birthdate	acquisition_year	no_of_orders	customer_lifetime_value
+11000	AW00011000	Jon	Yang	Australia	Married	1971-10-06	2011	3	8249
+11001	AW00011001	Eugene	Huang	Australia	Single	1976-05-10	2011	3	6384
+11002	AW00011002	Ruben	Torres	Australia	Married	1971-02-09	2011	3	8114
+11003	AW00011003	Christy	Zhu	Australia	Single	1973-08-14	2010	3	8139
+11004	AW00011004	Elizabeth	Johnson	Australia	Single	1979-08-05	2011	3	8196
+11005	AW00011005	Julio	Ruiz	Australia	Single	1976-08-01	2010	3	8121
+11006	AW00011006	Janet	Alvarez	Australia	Single	1976-12-02	2011	3	8119
+11007	AW00011007	Marco	Mehta	Australia	Married	1969-11-06	2011	3	8211
+11008	AW00011008	Rob	Verhoff	Australia	Single	1975-07-04	2011	3	8106
+11009	AW00011009	Shannon	Carlson	Australia	Single	1969-09-29	2011	3	8091
+*/
 
 
 
