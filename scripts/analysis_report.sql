719d62b420c1c63595f43d8ed2afe4dafb54b68e
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-01-30 19:49:13 -0500

    Update analysis_report.sql

--- scripts/analysis_report.sql
+++ scripts/analysis_report.sql
@@ -1,18 +1,20 @@
 /*Queries and Analysis*/
-
+-- ====================================================================================================================================
 -- >> Explore 
 -- Where Data was generated ?
 -- Databases which data is stored?
-
-/* Data was generated as result of business process of a E-commerce web based retailer which specializes on selling products related to
+-- ------------------------------------------------------------------------------------------------------------------------------------
+-- Solution
+/* 
+1)  Data was generated as result of business process of a E-commerce web based retailer which specializes on selling products related to
 sports such as bikes, clothings, gears and so on.
-The data is stored in the DataWarehouse database which follows a medallion architecture.
+2) The data is stored in the DataWarehouse database which follows a medallion architecture.
 The cleaned and analysis ready data is stored in the gold layer of the databse, as 3 objects or views.
 - dim.customers
 - dim.products
 - fact_sales
 */
-
+-- ===================================================================================================================================
 -- >> Profile
 -- Identifying the domain the data or business falls in
 -- Checking unique values, distributions of records in dataset
