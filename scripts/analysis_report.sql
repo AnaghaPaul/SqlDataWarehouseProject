a9adacd92f3e43367dec6f80e8abd5b9cf8e1aaa
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-01-31 21:06:40 -0500

    Update analysis_report.sql

--- scripts/analysis_report.sql
+++ scripts/analysis_report.sql
@@ -89,6 +89,7 @@ if required in the future, handled explicitly in ETL or stored as a separate der
 -- whether there are nulls
 -- whether negative values exist alongside positive ones
 -- --------------------------------------------------------------------------------------------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>- gold.fact_sales ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 -- order_number
 SELECT order_number,
 	 COUNT(*) AS frequency
@@ -108,7 +109,74 @@ SO65048			2
 -- Insight >> Order_number is not a unique field, multiple products bought in single order is represented as multiple records.
 -- 		   >> This is the reason for the duplicates in the field.
 -- ----------------------------------------------------------------------------------------------------------------------------------------
-
+-- product_key (Frequency Distribution)
+SELECT
+	product_key,
+COUNT(*) AS frequency
+FROM gold.fact_sales
+GROUP BY
+product_key;
+-- The results can be visualized through techniques including stem-and-leaf plots, box plots, and histograms.
+-- Result (Note : Limited to 5 rows)
+/*
+product_key	frequency
+46			53
+115			48
+138			232
+284			1396
+161			147
+*/
+-- ---------------------------------------------------------------------------------------------------------------------------------------
+-- customer_key (Frequency Distribution)
+SELECT 
+	customer_key,
+COUNT(*) AS frequency
+FROM gold.fact_sales
+GROUP BY
+customer_key;
+-- The results can be visualized through techniques including stem-and-leaf plots, box plots, and histograms.
+-- Result (Note : Limited to 5 rows)
+/*
+customer_key	frequency
+5621			3
+5692			4
+451				9
+4696			8
+1447			4
+*/
+-- ---------------------------------------------------------------------------------------------------------------------------------------
+-- sales_amount (continuous variable) - needs binning
+SELECT
+  ntile,
+  MIN(order_sales) AS lower_bound,
+  MAX(order_sales) AS upper_bound,
+  COUNT(*) AS orders
+FROM (
+  SELECT
+    order_number,
+    SUM(sales_amount) AS order_sales,
+    NTILE(10) OVER (ORDER BY SUM(sales_amount)) AS ntile
+  FROM gold.fact_sales
+  GROUP BY order_number
+) a
+GROUP BY ntile;
+/*
+-- The results can be visualized through techniques including stem-and-leaf plots, box plots, and histograms.
+-- Result (Note : Limited to 5 rows)
+-- >> Result :
+ntile	lower_bound	upper_bound	orders
+1	2	24	2766
+2	24	38	2766
+3	38	63	2766
+4	63	94	2766
+5	94	595	2766
+6	595	1000	2766
+7	1000	2049	2766
+8	2049	2352	2766
+9	2352	2451	2766
+10	2451	3578	2765
+*/
+-- -------------------------------------------------------------------------------------------------------------------------------------------
 	 
 SELECT 
 category, YEAR(fs.order_date) AS order_year,
