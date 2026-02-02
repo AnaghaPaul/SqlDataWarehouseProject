ed5d796ac2def23a4d4d5d1ae9802f850c07fc5d
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-02 14:05:54 -0500

    Update explore.sql

--- scripts/analysis/explore/explore.sql
+++ scripts/analysis/explore/explore.sql
@@ -1,1 +1,17 @@
-
+-- ===================================================================================================================================
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.Queries and Analysis*/>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- ====================================================================================================================================
+-- >> Explore 
+-- Where Data was generated ?
+-- Databases which data is stored?
+-- ------------------------------------------------------------------------------------------------------------------------------------
+-- Solution
+/* 
+1)  Data was generated as result of business process of a E-commerce web based retailer which specializes on selling products related to
+sports such as bikes, clothings, gears and so on.
+2) The data is stored in the DataWarehouse database which follows a medallion architecture.
+The cleaned and analysis ready data is stored in the gold layer of the databse, as 3 objects or views.
+- dim.customers
+- dim.products
+- fact_sales
+*/
