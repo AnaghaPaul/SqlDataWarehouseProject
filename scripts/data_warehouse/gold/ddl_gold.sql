cc5c004e0c0fb7f7253065f95af28e54e401b718
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-11 16:01:43 -0500

    Update ddl_gold.sql

--- scripts/data_warehouse/gold/ddl_gold.sql
+++ scripts/data_warehouse/gold/ddl_gold.sql
@@ -105,36 +105,36 @@ IF OBJECT_ID('gold.dim_order_date','V') IS NOT NULL
 GO
   CREATE VIEW gold.dim_order_date AS
   SELECT
-  	DateKey AS order_date_key, 
-    Date AS order_date,
-    FullDate AS order_full_date,-- Date in MM-dd-yyyy format
-    DayOfMonth AS order_day_of_month, -- Field will hold day number of Month
-    DaySuffix AS order_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
-    DayName AS order_day_name, -- Contains name of the day, Sunday, Monday 
-    DayOfWeek AS order_day_of_week,-- First Day Sunday=1 and Saturday=7
-    DayOfWeekInMonth AS order_day_of_week_in_month, --1st Monday or 2nd Monday in Month
-    DayOfWeekInYear AS order_day_of_week_in_year,
-    DayOfQuarter AS order_day_of_quarter, 
-    DayOfYear AS order_day_of_year,
-    WeekOfMonth AS order_week_of_month,-- Week Number of Month 
-    WeekOfQuarter AS order_week_of_quarter, --Week Number of the Quarter
-    WeekOfYear AS order_week_of_year,--Week Number of the Year
-    Month AS order_month, --Number of the Month 1 to 12
-    MonthName AS order_month_name,--January, February etc
-    MonthOfQuarter AS order_month_of_quarter,-- Month Number belongs to Quarter
-    Quarter AS order_quarter,
-    QuarterName AS order_quarter_name,--First,Second..
-    Year AS order_year,-- Year value of Date stored in Row
-    YearName AS order_year_name, --CY 2012,CY 2013
-    MonthYear AS order_month_year, --Jan-2013,Feb-2013
-    MMYYYY AS order_MMYYY,
-    FirstDayOfMonth AS order_first_day_of_month,
-    LastDayOfMonth AS order_last_day_of_month,
-    FirstDayOfQuarter AS order_first_day_of_quarter,
-    LastDayOfQuarter AS order_last_day_of_quarter,
-    FirstDayOfYear AS order_first_day_of_year,
-    LastDayOfYear AS order_last_day_of_year,
-    IsWeekday AS order_is_weekday-- 0=Week End ,1=Week Day
+  	date_key AS order_date_key, 
+    date AS order_date,
+    full_date AS order_full_date,-- Date in MM-dd-yyyy format
+    day_of_month AS order_day_of_month, -- Field will hold day number of Month
+    day_suffix AS order_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
+    day_name AS order_day_name, -- Contains name of the day, Sunday, Monday 
+    day_of_week AS order_day_of_week,-- First Day Sunday=1 and Saturday=7
+    day_of_week_in_month AS order_day_of_week_in_month, --1st Monday or 2nd Monday in Month
+    day_of_week_in_year AS order_day_of_week_in_year,
+    day_of_quarter AS order_day_of_quarter, 
+    day_of_year AS order_day_of_year,
+    week_of_month AS order_week_of_month,-- Week Number of Month 
+    week_of_quarter AS order_week_of_quarter, --Week Number of the Quarter
+    week_of_year AS order_week_of_year,--Week Number of the Year
+    month AS order_month, --Number of the Month 1 to 12
+    month_name AS order_month_name,--January, February etc
+    month_of_quarter AS order_month_of_quarter,-- Month Number belongs to Quarter
+    quarter AS order_quarter,
+    quarter_name AS order_quarter_name,--First,Second..
+    year AS order_year,-- Year value of Date stored in Row
+    year_name AS order_year_name, --CY 2012,CY 2013
+    month_year AS order_month_year, --Jan-2013,Feb-2013
+    mmyyyy AS order_mmyyyy,
+    first_day_of_month AS order_first_day_of_month,
+    last_day_of_month AS order_last_day_of_month,
+    first_day_of_quarter AS order_first_day_of_quarter,
+    last_day_of_quarter AS order_last_day_of_quarter,
+    first_day_of_year AS order_first_day_of_year,
+    last_day_of_year AS order_last_day_of_year,
+    is_weekday AS order_is_weekday-- 0=Week End ,1=Week Day
 	
   FROM silver.dwh_dim_date
 
@@ -146,36 +146,36 @@ IF OBJECT_ID('gold.dim_shipping_date','V') IS NOT NULL
 GO
   CREATE VIEW gold.dim_shipping_date AS
   SELECT
-  	DateKey AS shipping_date_key, 
-    Date AS shipping_date,
-    FullDate AS shipping_full_date,-- Date in MM-dd-yyyy format
-    DayOfMonth AS shipping_day_of_month, -- Field will hold day number of Month
-    DaySuffix AS shipping_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
-    DayName AS shipping_day_name, -- Contains name of the day, Sunday, Monday 
-    DayOfWeek AS shipping_day_of_week,-- First Day Sunday=1 and Saturday=7
-    DayOfWeekInMonth AS shipping_day_of_week_in_month, --1st Monday or 2nd Monday in Month
-    DayOfWeekInYear AS shipping_day_of_week_in_year,
-    DayOfQuarter AS shipping_day_of_quarter, 
-    DayOfYear AS shipping_day_of_year,
-    WeekOfMonth AS shipping_week_of_month,-- Week Number of Month 
-    WeekOfQuarter AS shipping_week_of_quarter, --Week Number of the Quarter
-    WeekOfYear AS shipping_week_of_year,--Week Number of the Year
-    Month AS shipping_month, --Number of the Month 1 to 12
-    MonthName AS shipping_month_name,--January, February etc
-    MonthOfQuarter AS shipping_month_of_quarter,-- Month Number belongs to Quarter
-    Quarter AS shipping_quarter,
-    QuarterName AS shipping_quarter_name,--First,Second..
-    Year AS shipping_year,-- Year value of Date stored in Row
-    YearName AS shipping_year_name, --CY 2012,CY 2013
-    MonthYear AS shipping_month_year, --Jan-2013,Feb-2013
-    MMYYYY AS shipping_MMYYY,
-    FirstDayOfMonth AS shipping_first_day_of_month,
-    LastDayOfMonth AS shipping_last_day_of_month,
-    FirstDayOfQuarter AS shipping_first_day_of_quarter,
-    LastDayOfQuarter AS shipping_last_day_of_quarter,
-    FirstDayOfYear AS shipping_first_day_of_year,
-    LastDayOfYear AS shipping_last_day_of_year,
-    IsWeekday AS shipping_is_weekday-- 0=Week End ,1=Week Day
+  	date_key AS shipping_date_key, 
+    date AS shipping_date,
+    full_date AS shipping_full_date,-- Date in MM-dd-yyyy format
+    day_of_month AS shipping_day_of_month, -- Field will hold day number of Month
+    day_suffix AS shipping_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
+    day_name AS shipping_day_name, -- Contains name of the day, Sunday, Monday 
+    day_of_week AS shipping_day_of_week,-- First Day Sunday=1 and Saturday=7
+    day_of_week_in_month AS shipping_day_of_week_in_month, --1st Monday or 2nd Monday in Month
+    day_of_week_in_year AS shipping_day_of_week_in_year,
+    day_of_quarter AS shipping_day_of_quarter, 
+    day_of_year AS shipping_day_of_year,
+    week_of_month AS shipping_week_of_month,-- Week Number of Month 
+    week_of_quarter AS shipping_week_of_quarter, --Week Number of the Quarter
+    week_of_year AS shipping_week_of_year,--Week Number of the Year
+    month AS shipping_month, --Number of the Month 1 to 12
+    month_name AS shipping_month_name,--January, February etc
+    month_of_quarter AS shipping_month_of_quarter,-- Month Number belongs to Quarter
+    quarter AS shipping_quarter,
+    quarter_name AS shipping_quarter_name,--First,Second..
+    year AS shipping_year,-- Year value of Date stored in Row
+    year_name AS shipping_year_name, --CY 2012,CY 2013
+    month_year AS shipping_month_year, --Jan-2013,Feb-2013
+    mmyyyy AS shipping_mmyyyy,
+    first_day_of_month AS shipping_first_day_of_month,
+    last_day_of_month AS shipping_last_day_of_month,
+    first_day_of_quarter AS shipping_first_day_of_quarter,
+    last_day_of_quarter AS shipping_last_day_of_quarter,
+    first_day_of_year AS shipping_first_day_of_year,
+    last_day_of_year AS shipping_last_day_of_year,
+    is_weekday AS shipping_is_weekday-- 0=Week End ,1=Week Day
 	
   FROM silver.dwh_dim_date
 -- ---------------------------------------------------------------------------------------------------------------------------------
@@ -186,35 +186,35 @@ IF OBJECT_ID('gold.dim_due_date','V') IS NOT NULL
 GO
   CREATE VIEW gold.dim_due_date AS
   SELECT
-  	DateKey AS due_date_key, 
-    Date AS due_date,
-    FullDate AS due_full_date,-- Date in MM-dd-yyyy format
-    DayOfMonth AS due_day_of_month, -- Field will hold day number of Month
-    DaySuffix AS due_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
-    DayName AS due_day_name, -- Contains name of the day, Sunday, Monday 
-    DayOfWeek AS due_day_of_week,-- First Day Sunday=1 and Saturday=7
-    DayOfWeekInMonth AS due_day_of_week_in_month, --1st Monday or 2nd Monday in Month
-    DayOfWeekInYear AS due_day_of_week_in_year,
-    DayOfQuarter AS due_day_of_quarter, 
-    DayOfYear AS due_day_of_year,
-    WeekOfMonth AS due_week_of_month,-- Week Number of Month 
-    WeekOfQuarter AS due_week_of_quarter, --Week Number of the Quarter
-    WeekOfYear AS due_week_of_year,--Week Number of the Year
-    Month AS due_month, --Number of the Month 1 to 12
-    MonthName AS due_month_name,--January, February etc
-    MonthOfQuarter AS due_month_of_quarter,-- Month Number belongs to Quarter
-    Quarter AS due_quarter,
-    QuarterName AS due_quarter_name,--First,Second..
-    Year AS due_year,-- Year value of Date stored in Row
-    YearName AS due_year_name, --CY 2012,CY 2013
-    MonthYear AS due_month_year, --Jan-2013,Feb-2013
-    MMYYYY AS due_MMYYY,
-    FirstDayOfMonth AS due_first_day_of_month,
-    LastDayOfMonth AS due_last_day_of_month,
-    FirstDayOfQuarter AS due_first_day_of_quarter,
-    LastDayOfQuarter AS due_last_day_of_quarter,
-    FirstDayOfYear AS due_first_day_of_year,
-    LastDayOfYear AS due_last_day_of_year,
-    IsWeekday AS due_is_weekday-- 0=Week End ,1=Week Day
+  	date_key AS due_date_key, 
+    date AS due_date,
+    full_date AS due_full_date,-- Date in MM-dd-yyyy format
+    day_of_month AS due_day_of_month, -- Field will hold day number of Month
+    day_suffix AS due_day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
+    day_name AS due_day_name, -- Contains name of the day, Sunday, Monday 
+    day_of_week AS due_day_of_week,-- First Day Sunday=1 and Saturday=7
+    day_of_week_in_month AS due_day_of_week_in_month, --1st Monday or 2nd Monday in Month
+    day_of_week_in_year AS due_day_of_week_in_year,
+    day_of_quarter AS due_day_of_quarter, 
+    day_of_year AS due_day_of_year,
+    week_of_month AS due_week_of_month,-- Week Number of Month 
+    week_of_quarter AS due_week_of_quarter, --Week Number of the Quarter
+    week_of_year AS due_week_of_year,--Week Number of the Year
+    month AS due_month, --Number of the Month 1 to 12
+    month_name AS due_month_name,--January, February etc
+    month_of_quarter AS due_month_of_quarter,-- Month Number belongs to Quarter
+    quarter AS due_quarter,
+    quarter_name AS due_quarter_name,--First,Second..
+    year AS due_year,-- Year value of Date stored in Row
+    year_name AS due_year_name, --CY 2012,CY 2013
+    month_year AS due_month_year, --Jan-2013,Feb-2013
+    mmyyyy AS due_mmyyyy,
+    first_day_of_month AS due_first_day_of_month,
+    last_day_of_month AS due_last_day_of_month,
+    first_day_of_quarter AS due_first_day_of_quarter,
+    last_day_of_quarter AS due_last_day_of_quarter,
+    first_day_of_year AS due_first_day_of_year,
+    last_day_of_year AS due_last_day_of_year,
+    is_weekday AS due_is_weekday-- 0=Week End ,1=Week Day
 	
   FROM silver.dwh_dim_date
