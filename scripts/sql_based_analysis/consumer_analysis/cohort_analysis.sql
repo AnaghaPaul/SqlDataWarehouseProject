bbf0f8cf0e1462120eafdb636bfb9d53c65c76ec
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-24 23:14:24 -0500

    Update cohort_analysis.sql

--- scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
+++ scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
@@ -1,1 +1,25 @@
+-- __________________________________Cohort Analysis_________________________________________
+/* A Cohort Analysis compares similar groups over time.*/
+-- Type of cohort analysis - Returnship - Repeat Purchase Behaviour
+SELECT
+first_shipping_year AS cohort_year,
+COUNT(customer_key) AS customers
+FROM
+(SELECT
+f.customer_key AS customer_key,
+MIN(s.shipping_year) AS first_shipping_year
+FROM
+gold.fact_sales AS f
+JOIN
+gold.dim_shipping_date  AS s
+ON f.shipping_date_key = s.shipping_date_key
+GROUP BY f.customer_key)a
+GROUP BY first_shipping_year;
+/*
+cohort_year	customers
+2011	      2178
+2012	      3220
+2013	      12371
+2014	      715
+*/
 
