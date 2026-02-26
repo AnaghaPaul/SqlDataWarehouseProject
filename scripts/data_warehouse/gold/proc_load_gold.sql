c56a6cd020feb953e1049c63ff1a24d534191a06
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-26 13:03:36 -0500

    Update proc_load_gold.sql

--- scripts/data_warehouse/gold/proc_load_gold.sql
+++ scripts/data_warehouse/gold/proc_load_gold.sql
@@ -51,18 +51,18 @@ CREATE OR ALTER PROCEDURE gold.load_gold AS -- stored procedure
   	create_date
   )
   SELECT
-    		ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
-    		ci.cst_id AS customer_id,
-    		ci.cst_key AS customer_number,
-    		ci.cst_firstname AS first_name,
-    		ci.cst_lastname AS last_name,
-    		la.cntry AS country,
-    		ci.cst_marital_status AS marital_status,
+    		ROW_NUMBER() OVER (ORDER BY cst_id),
+    		ci.cst_id,
+    		ci.cst_key,
+    		ci.cst_firstname,
+    		ci.cst_lastname,
+    		la.cntry,
+    		ci.cst_marital_status,
     		CASE  WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master for gender info
     			  ELSE COALESCE(ca.gen,'n/a')
-    		END AS gender,
-    		ca.bdate AS birthdate,
-    		ci.cst_create_date AS create_date	
+    		END,
+    		ca.bdate,
+    		ci.cst_create_date	
     FROM silver.crm_cust_info ci
     LEFT JOIN silver.erp_cust_az12 ca
     ON		  ci.cst_key = ca.cid
@@ -97,17 +97,17 @@ CREATE OR ALTER PROCEDURE gold.load_gold AS -- stored procedure
 	start_date
 	)
 	SELECT
-	  	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, 
-	  	pn.prd_id AS product_id,
-	  	pn.prd_key AS product_number,
-	  	pn.prd_name AS product_name,
-	  	pn.cat_id AS category_id,
-	  	pc.cat AS category,
-	  	pc.subcat AS subcategory,
+	  	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key), 
+	  	pn.prd_id,
+	  	pn.prd_key,
+	  	pn.prd_name,
+	  	pn.cat_id,
+	  	pc.cat,
+	  	pc.subcat,
 	  	pc.maintenance,
-	  	pn.prd_cost AS cost,
-	  	pn.prd_line AS product_line,
-	  	pn.prd_start_dt AS start_date
+	  	pn.prd_cost,
+	  	pn.prd_line,
+	  	pn.prd_start_dt
 	  FROM silver.crm_prd_info pn
 	  LEFT JOIN silver.erp_px_cat_g1v2 pc
 	  ON        pn.cat_id = pc.id
@@ -283,6 +283,25 @@ CREATE OR ALTER PROCEDURE gold.load_gold AS -- stored procedure
 	PRINT '=================================================================================================';
 	PRINT '>> Fact and all Dimensions table loaded successfully.';
 	PRINT '=================================================================================================';
+
+	SET @batch_end_time=GETDATE();
+			PRINT '---------------------------------------------------------------------------------------------------------------'
+			PRINT '>>Loading gold layer is completed'
+			PRINT '---------------------------------------------------------------------------------------------------------------'
+	
+			PRINT '>>Total Load Duration for gold layer:' +CAST(DATEDIFF(second,@batch_start_time,@batch_end_time) AS NVARCHAR)+ ' seconds';
+			PRINT '---------------------------------------------------------------------------------------------------------------'
+
+		END TRY
+		BEGIN CATCH
+			 PRINT '==============================================='
+			 PRINT 'ERROR OCCURED WHILE LOADING THE GOLD LAYER'
+			 PRINT ' ERROR MESSAGE ' + ERROR_MESSAGE();
+			 PRINT ' ERROR MESSAGE ' +CAST(ERROR_NUMBER() AS NVARCHAR);
+			 PRINT ' ERROR MESSAGE ' +CAST(ERROR_STATE() AS NVARCHAR);
+			 PRINT '==============================================='
+		END CATCH			
+	END
 
 
 	
