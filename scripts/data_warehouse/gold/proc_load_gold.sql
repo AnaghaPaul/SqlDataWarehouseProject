2ff772fdf07e4daafac7dac9ff1ab4ee79fad203
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-26 12:21:59 -0500

    Update proc_load_gold.sql

--- scripts/data_warehouse/gold/proc_load_gold.sql
+++ scripts/data_warehouse/gold/proc_load_gold.sql
@@ -118,6 +118,140 @@ CREATE OR ALTER PROCEDURE gold.load_gold AS -- stored procedure
 	PRINT '>>Load Duration:' +CAST(DATEDIFF(second,@start_load_time,@end_load_time) AS NVARCHAR)+ ' seconds';
 	PRINT '-------------------------------------------------------------------------------------------------';
 -- ---------------------------------------------------------------------------------------------------------------------
+	PRINT '>>Preparing to truncate and load gold.dim_date';
+	SET @start_load_time = GETDATE();
+-- Truncate
+	PRINT'>> Truncating Table : gold.dim_date';
+	TRUNCATE TABLE gold.dim_date;
+	PRINT'>> Inserting Data Into : gold.dim_date';
+-- Insert
+	INSERT INTO gold.dim_date
+	(
+	    date_key, 
+	    date,
+	    full_date,-- Date in MM-dd-yyyy format
+	    day_of_month, -- Field will hold day number of Month
+	    day_suffix, -- Apply suffix as 1st, 2nd ,3rd etc
+	    day_name, -- Contains name of the day, Sunday, Monday 
+	    day_of_week,-- First Day Sunday=1 and Saturday=7
+	    day_of_week_in_month, --1st Monday or 2nd Monday in Month
+	    day_of_week_in_year,
+	    day_of_quarter, 
+	    day_of_year,
+	    week_of_month,-- Week Number of Month 
+	    week_of_quarter, --Week Number of the Quarter
+	    week_of_year,--Week Number of the Year
+	    month, --Number of the Month 1 to 12
+	    month_name,--January, February etc
+	    month_of_quarter,-- Month Number belongs to Quarter
+	    quarter,
+	    quarter_name,--First,Second..
+	    year,-- Year value of Date stored in Row
+	    year_name, --CY 2012,CY 2013
+	    month_year, --Jan-2013,Feb-2013
+	    mmyyyy,
+	    first_day_of_month,
+	    last_day_of_month,
+	    first_day_of_quarter,
+	    last_day_of_quarter,
+	    first_day_of_year,
+	    last_day_of_year,
+		season,
+		is_holiday,-- Flag 1=Global Holiday, 0-No Global Holiday
+	    is_weekday,-- 0=Week End ,1=Week Day
+		holiday_name,--Name of Global
+		fiscal_day_of_year,
+		fiscal_week_of_year,
+		fiscal_month,
+		fiscal_quarter,
+		fiscal_quarter_name,
+		fiscal_year,
+		fiscal_year_name,
+		fiscal_month_year,
+		fiscal_mmyyyy,
+		fiscal_first_day_of_month,
+		fiscal_last_day_of_month,
+		fiscal_first_day_of_quarter,
+		fiscal_last_day_of_quarter,
+		fiscal_first_day_of_year,
+		fiscal_last_day_of_year
+	)
+	SELECT
+		date_key, 
+	    date,
+	    full_date,
+	    day_of_month,
+	    day_suffix,
+	    day_name,
+	    day_of_week,
+	    day_of_week_in_month,
+	    day_of_week_in_year,
+	    day_of_quarter, 
+	    day_of_year,
+	    week_of_month,
+	    week_of_quarter,
+	    week_of_year,
+	    month,
+	    month_name,
+	    month_of_quarter,
+	    quarter,
+	    quarter_name,
+	    year,
+	    year_name,
+	    month_year,
+	    mmyyyy,
+	    first_day_of_month,
+	    last_day_of_month,
+	    first_day_of_quarter,
+	    last_day_of_quarter,
+	    first_day_of_year,
+	    last_day_of_year,
+		season,
+		is_holiday,
+	    is_weekday,
+		holiday_name,
+		fiscal_day_of_year,
+		fiscal_week_of_year,
+		fiscal_month,
+		fiscal_quarter,
+		fiscal_quarter_name,
+		fiscal_year,
+		fiscal_year_name,
+		fiscal_month_year,
+		fiscal_mmyyyy,
+		fiscal_first_day_of_month,
+		fiscal_last_day_of_month,
+		fiscal_first_day_of_quarter,
+		fiscal_last_day_of_quarter,
+		fiscal_first_day_of_year,
+		fiscal_last_day_of_year
+	FROM silver.dwh_dim_date
+	SET @end_load_time = GETDATE();
+	PRINT '>>Loading to gold.dim_date completed successfully';
+	PRINT '>>Load Duration:' +CAST(DATEDIFF(second,@start_load_time,@end_load_time) AS NVARCHAR)+ ' seconds';
+	PRINT '-------------------------------------------------------------------------------------------------';
+
+	PRINT '>> Dimension Tables loaded successfully.';
+	PRINT '=================================================================================================';
+-- =========================================================================================================================
+	--Sales Fact
+	PRINT '------------------------------------------';
+	PRINT 'Loading Fact Table';
+	PRINT '------------------------------------------';
+
+	-- gold.fact_sales
+	PRINT '>>Preparing to truncate and load gold.fact_sales';
+	SET @start_load_time = GETDATE();
+-- Truncate
+	PRINT'>> Truncating Table : gold.fact_sales';
+	TRUNCATE TABLE gold.fact_sales;
+	PRINT'>> Inserting Data Into : gold.fact_sales';
+-- Insert
+
+	
+	
+	
+
 
 
 	
