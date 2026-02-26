7370e9e2b5debe04521533647d1f8478a915e903
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-26 09:31:06 -0500

    Update ddl_gold.sql

--- scripts/data_warehouse/gold/ddl_gold.sql
+++ scripts/data_warehouse/gold/ddl_gold.sql
@@ -18,27 +18,26 @@ Usage:
 -- gold.dim_customers
 IF OBJECT_ID('gold.dim_customers','V') IS NOT NULL
     DROP VIEW gold.dim_customers;
-GO
-IF OBJECT_ID('gold.fact_sales','V') IS NOT NULL
-    DROP VIEW gold.fact_sales;
-GO
---gold.fact_sales
-CREATE VIEW gold.fact_sales AS
-  SELECT 
-    sd.sls_ord_num AS order_number,
-    pr.product_key,
-    cu.customer_key, 
-    YEAR(sd.sls_order_dt) * 10000 + MONTH(sd.sls_order_dt) * 100 + DAY(sd.sls_order_dt) AS order_date_key,
-    YEAR(sd.sls_ship_dt) * 10000 + MONTH(sd.sls_ship_dt)*100 +DAY(sd.sls_ship_dt) AS shipping_date_key,
-    YEAR(sd.sls_due_dt) * 10000 + MONTH(sd.sls_due_dt)*100 + DAY(sd.sls_due_dt) AS due_date_key,
-    sd.sls_sales AS sales_amount,
-    sd.sls_quantity AS quantity,
-    sd.sls_price AS price
-  FROM silver.crm_sales_details sd
-  LEFT JOIN gold.dim_products pr
-  ON		  sd.sls_prd_key = pr.product_number
-  LEFT JOIN gold.dim_customers cu
-  ON		  sd.sls_cust_id = cu.customer_id;
+
+CREATE VIEW gold.dim_customers AS
+  SELECT
+  		ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+  		ci.cst_id AS customer_id,
+  		ci.cst_key AS customer_number,
+  		ci.cst_firstname AS first_name,
+  		ci.cst_lastname AS last_name,
+  		la.cntry AS country,
+  		ci.cst_marital_status AS marital_status,
+  		CASE  WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master for gender info
+  			  ELSE COALESCE(ca.gen,'n/a')
+  		END AS gender,
+  		ca.bdate AS birthdate,
+  		ci.cst_create_date AS create_date	
+  FROM silver.crm_cust_info ci
+  LEFT JOIN silver.erp_cust_az12 ca
+  ON		  ci.cst_key = ca.cid
+  LEFT JOIN silver.erp_loc_a101 la
+  ON        ci.cst_key = la.cid;
 
 -- =================================================================================
 -- Create Dimension : gold.dim_products
