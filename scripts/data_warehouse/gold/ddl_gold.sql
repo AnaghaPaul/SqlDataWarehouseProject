651c16617b8024c0613ddba8e4d8241cb9ab3744
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-02-26 09:47:55 -0500

    Update ddl_gold.sql

--- scripts/data_warehouse/gold/ddl_gold.sql
+++ scripts/data_warehouse/gold/ddl_gold.sql
@@ -1,1 +1,16 @@
-
+-- gold.dim_customers
+IF OBJECT_ID('gold.dim_customers','U') IS NOT NULL
+	DROP TABLE gold.dim_customers;
+GO
+CREATE TABLE gold.dim_customers(
+customer_key INT,
+customer_id INT,
+customer_number NVARCHAR(50),
+first_name NVARCHAR(50),
+last_name NVARCHAR(50),
+country NVARCHAR(50),
+marital_status VARCHAR(10),
+gender VARCHAR(20),
+birthdate DATE,
+create_date DATE
+);
