72eff039c9641f40c7199ed5b69dcd7d851c3c15
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-03-17 11:40:25 -0400

    Update cohort_analysis.sql

--- scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
+++ scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
@@ -138,86 +138,116 @@ WITH customer_cohort AS
 (
     SELECT
         f.customer_key,
-        MIN(o.order_year)  AS first_order_year,
-        MIN(o.order_month) AS first_order_month
+        MIN(f.order_date_key) AS first_order_date_key
     FROM gold.fact_sales f
-    JOIN gold.dim_order_date o
-        ON f.order_date_key = o.order_date_key
     GROUP BY f.customer_key
-),
-
-month_lookup AS
-(
-    SELECT DISTINCT
-        order_month,
-        order_month_name
-    FROM gold.dim_order_date
 )
 
 SELECT
-    m.order_month_name,
+    d.order_year,
+    d.order_month,
+    d.order_month_name,
     COUNT(DISTINCT c.customer_key) AS total_customers
 FROM customer_cohort c
-JOIN month_lookup m
-    ON c.first_order_month = m.order_month
-WHERE c.first_order_year = 2013
+JOIN gold.dim_order_date d
+    ON c.first_order_date_key = d.order_date_key
+WHERE d.order_year = 2013
 GROUP BY
-    c.first_order_month,
-    m.order_month_name
+    d.order_year,
+    d.order_month,
+    d.order_month_name
 ORDER BY
-    c.first_order_month
-;
+    d.order_month;
 /*
-order_month_name	total_customers
-January				636
-February			1039
-March				1131
-April				1055
-May					1107
-June				1132
-July				1027
-August				1050
-September			1005
-October				1106
-November			1106
-December			1127
+order_year	order_month	order_month_name	total_customers
+2013		1			January				324
+2013		2			February			1087
+2013		3			March				1164
+2013		4			April				1088
+2013		5			May					1141
+2013		6			June				1154
+2013		7			July				1052
+2013		8			August				1063
+2013		9			September			1029
+2013		10			October				1133
+2013		11			November			1133
+2013		12			December			1142
 */
 
 WITH customer_cohort AS
 (
-SELECT
-    f.customer_key,
-    MIN(o.order_year)  AS first_order_year,
-    MIN(o.order_month) AS first_order_month
-FROM gold.fact_sales f
-JOIN gold.dim_order_date o
-    ON f.order_date_key = o.order_date_key
-GROUP BY f.customer_key
+    SELECT
+        f.customer_key,
+        MIN(f.order_date_key) AS first_order_date_key
+    FROM gold.fact_sales f
+    GROUP BY f.customer_key
 )
+, cohort_with_date AS
+(
+    SELECT
+        c.customer_key,
+        d.order_year,
+        d.order_month,
+        d.order_month_name,
+        c.first_order_date_key
+    FROM customer_cohort c
+    JOIN gold.dim_order_date d
+        ON c.first_order_date_key = d.order_date_key
+)
+, cohort_activity AS
+(
+    SELECT
+        c.customer_key,
+        c.order_year,
+        c.order_month AS cohort_month,
 
+        d.order_year AS activity_year,
+        d.order_month AS activity_month,
+
+     
+        (d.order_year - c.order_year) * 12 +
+        (d.order_month - c.order_month) AS month_offset,
+
+        s.sales_amount
+    FROM cohort_with_date c
+    JOIN gold.fact_sales s
+        ON c.customer_key = s.customer_key
+    JOIN gold.dim_order_date d
+        ON s.order_date_key = d.order_date_key
+    WHERE c.order_year = 2013
+)
 SELECT
-    c.first_order_month,
+    cohort_month,
 
-    SUM(CASE WHEN o.order_month = 1  THEN s.sales_amount ELSE 0 END) AS revenue_jan,
-    SUM(CASE WHEN o.order_month = 2  THEN s.sales_amount ELSE 0 END) AS revenue_feb,
-    SUM(CASE WHEN o.order_month = 3  THEN s.sales_amount ELSE 0 END) AS revenue_mar,
-    SUM(CASE WHEN o.order_month = 4  THEN s.sales_amount ELSE 0 END) AS revenue_apr,
-    SUM(CASE WHEN o.order_month = 5  THEN s.sales_amount ELSE 0 END) AS revenue_may,
-    SUM(CASE WHEN o.order_month = 6  THEN s.sales_amount ELSE 0 END) AS revenue_jun,
-    SUM(CASE WHEN o.order_month = 7  THEN s.sales_amount ELSE 0 END) AS revenue_jul,
-    SUM(CASE WHEN o.order_month = 8  THEN s.sales_amount ELSE 0 END) AS revenue_aug,
-    SUM(CASE WHEN o.order_month = 9  THEN s.sales_amount ELSE 0 END) AS revenue_sep,
-    SUM(CASE WHEN o.order_month = 10 THEN s.sales_amount ELSE 0 END) AS revenue_oct,
-    SUM(CASE WHEN o.order_month = 11 THEN s.sales_amount ELSE 0 END) AS revenue_nov,
-    SUM(CASE WHEN o.order_month = 12 THEN s.sales_amount ELSE 0 END) AS revenue_dec
+    SUM(CASE WHEN month_offset = 0 THEN sales_amount ELSE 0 END) AS M0,
+    SUM(CASE WHEN month_offset = 1 THEN sales_amount ELSE 0 END) AS M1,
+    SUM(CASE WHEN month_offset = 2 THEN sales_amount ELSE 0 END) AS M2,
+    SUM(CASE WHEN month_offset = 3 THEN sales_amount ELSE 0 END) AS M3,
+    SUM(CASE WHEN month_offset = 4 THEN sales_amount ELSE 0 END) AS M4,
+    SUM(CASE WHEN month_offset = 5 THEN sales_amount ELSE 0 END) AS M5,
+    SUM(CASE WHEN month_offset = 6 THEN sales_amount ELSE 0 END) AS M6,
+    SUM(CASE WHEN month_offset = 7 THEN sales_amount ELSE 0 END) AS M7,
+    SUM(CASE WHEN month_offset = 8 THEN sales_amount ELSE 0 END) AS M8,
+    SUM(CASE WHEN month_offset = 9 THEN sales_amount ELSE 0 END) AS M9,
+    SUM(CASE WHEN month_offset = 10 THEN sales_amount ELSE 0 END) AS M10,
+    SUM(CASE WHEN month_offset = 11 THEN sales_amount ELSE 0 END) AS M11
 
-FROM customer_cohort c
-JOIN gold.fact_sales s
-    ON c.customer_key = s.customer_key
-JOIN gold.dim_order_date o
-    ON s.order_date_key = o.order_date_key
+FROM cohort_activity
+GROUP BY cohort_month
+ORDER BY cohort_month;
 
-WHERE c.first_order_year = 2013
-
-GROUP BY c.first_order_month
-ORDER BY c.first_order_month;
+/*
+cohort_month	M0			M1			M2			M3			M4			M5			M6			M7			M8			M9			M10			M11
+1				316236		978			1050		6191		3593		3520		1774		6375		785			2052		6421		1114
+2				301976		4258		3779		8805		7959		4230		4997		3596		4375		4172		4085		3237
+3				339511		2405		2732		6678		14656		8828		2061		2453		2506		2132		1978		0
+4				403539		1562		2652		11249		1902		4475		1841		1333		1835		1705		0			0
+5				477508		2282		8656		13665		11092		1273		1822		1603		2164		0			0			0
+6				540536		1007		4156		6422		28103		6944		1573		1100		0			0			0			0
+7				351783		1155		1692		1597		3940		21640		1530		0			0			0			0			0
+8				388037		1025		3896		1546		14071		1487		0			0			0			0			0			0
+9				402529		1561		1520		1713		1373		0			0			0			0			0			0			0
+10				575330		1682		1440		1315		0			0			0			0			0			0			0			0
+11				745494		1587		1542		0			0			0			0			0			0			0			0			0
+12				736919		1048		0			0			0			0			0			0			0			0			0			0
+*/
